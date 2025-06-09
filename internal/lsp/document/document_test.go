02003f6cf0ba10b4e14648cf751379d737ba6a47
qvalentin <36446499+qvalentin@users.noreply.github.com>
2025-06-09 11:35:36 +0200

    fix(values): do values files detection on the server (#147)

--- internal/lsp/document/document_test.go
+++ internal/lsp/document/document_test.go
@@ -35,3 +35,50 @@ func TestDocumentStore(t *testing.T) {
 	assert.NotNil(doc)
 	assert.True(ok)
 }
+
+func TestDocumentDocumentTypeForFile(t *testing.T) {
+	testCases := []struct {
+		fileURI protocol.URI
+		langID  protocol.LanguageIdentifier
+		want    DocumentType
+	}{
+		{uri.File("test.yaml"), "helm", TemplateDocumentType},
+		{uri.File("somewhere/test.yaml"), "helm", TemplateDocumentType},
+		{uri.File("somewhere/test.yaml"), "helm-template", TemplateDocumentType},
+		{uri.File("test.yml"), "helm", TemplateDocumentType},
+		{uri.File("test.yaml"), "yaml", YamlDocumentType},
+		{uri.File("test.yml"), "yaml", YamlDocumentType},
+		{uri.File("values.yml"), "helm", YamlDocumentType},
+		{uri.File("values.dev.yml"), "helm", YamlDocumentType},
+		{uri.File("values.yaml"), "helm", YamlDocumentType},
+		{uri.File("values.dev.yaml"), "helm", YamlDocumentType},
+		{uri.File("templates/values.dev.yaml"), "helm", TemplateDocumentType},
+	}
+
+	for _, testCase := range testCases {
+		got := DocumentTypeForFile(testCase.langID, testCase.fileURI)
+		assert.Equal(t, testCase.want, got, "DocumentTypeForFile(%s, %s) = %s, want %s", testCase.langID, testCase.fileURI, got, testCase.want)
+	}
+}
+
+func TestIsValuesYamlFile(t *testing.T) {
+	testCases := []struct {
+		fileURI protocol.URI
+		want    bool
+	}{
+		{uri.File("values.yaml"), true},
+		{uri.File("values.yml"), true},
+		{uri.File("values.dev.yaml"), true},
+		{uri.File("values.dev.yml"), true},
+		{uri.File("values.yaml"), true},
+		{uri.File("values.dev.yaml"), true},
+		{uri.File("templates/values.dev.yaml"), false},
+		{uri.File("deployment.yaml"), false},
+		{uri.File("templates/deployment.yaml"), false},
+	}
+
+	for _, testCase := range testCases {
+		got := IsValuesYamlFile(testCase.fileURI)
+		assert.Equal(t, testCase.want, got, "IsValuesYamlFile(%s) = %t, want %t", testCase.fileURI, got, testCase.want)
+	}
+}
