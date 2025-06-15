e1f5667b3b733772eae74a00a2464d27a89bddf5
qvalentin <valentin.theodor@web.de>
2025-06-15 12:05:22 +0200

    wip: yaml file go-to-references

--- internal/handler/yaml_handler/hover.go
+++ internal/handler/yaml_handler/hover.go
@@ -2,26 +2,46 @@ package yamlhandler
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
+	"github.com/mrjosh/helm-ls/internal/protocol"
+	"github.com/mrjosh/helm-ls/internal/util"
 	lsp "go.lsp.dev/protocol"
 )
 
 // Hover implements handler.LangHandler.
 func (h *YamlHandler) Hover(ctx context.Context, params *lsp.HoverParams) (result *lsp.Hover, err error) {
+	yamlResult, yamllsErr := h.yamllsConnector.CallHover(ctx, *params)
+
+	path, err := h.GetYamlPath(params.TextDocument.URI, params.Position)
+
 	logger.Debug("YamlHandler Hover", params)
-	return h.yamllsConnector.CallHover(ctx, *params)
+
+	if yamlResult == nil {
+		return protocol.BuildHoverResponse(path, lsp.Range{}), errors.Join(yamllsErr, err)
+	}
+
+	yamlResult.Contents.Value = yamlResult.Contents.Value + "\n\n" + path
+
+	return yamlResult, errors.Join(yamllsErr, err)
+}
+
+func (h *YamlHandler) GetYamlPath(uri lsp.URI, pos lsp.Position) (path string, err error) {
 	// IDEA: return the json path of the current node
-	// doc, ok := h.documents.GetYamlDoc(params.TextDocument.URI)
-	//
-	// if !ok {
-	// 	return nil, fmt.Errorf("no document for %s", params.TextDocument.URI)
-	// }
-	//
-	// node := util.GetNodeForPosition(&doc.Node, params.Position)
-	//
-	// if node == nil {
-	// 	return nil, fmt.Errorf("no node found")
-	// }
-	//
-	// return protocol.BuildHoverResponse(node.Value, lsp.Range{}), doc.ParseErr
+	doc, ok := h.documents.GetYamlDoc(uri)
+
+	if !ok {
+		return "", fmt.Errorf("no document for %s", uri)
+	}
+
+	node := util.GetNodeForPosition2(doc.GoccyYamlNode, pos)
+
+	if node == nil {
+		return "", fmt.Errorf("no node found")
+	}
+
+	path = node.GetPath()
+
+	return path, nil
 }
--- internal/util/yaml_goccy_test.go
+++ internal/util/yaml_goccy_test.go
@@ -6,16 +6,13 @@ import (
 	"github.com/goccy/go-yaml"
 	"github.com/goccy/go-yaml/ast"
 	"github.com/stretchr/testify/assert"
+	"go.lsp.dev/protocol"
 )
 
-// Unit test for PositionFinderVisitor
 func TestPositionFinderVisitor(t *testing.T) {
-	// Example AST represented as a slice of Nodes (strings).
 	var node ast.Node
 
 	yml := `
-%YAML 1.2
----
 a: 
   World: something
 b: c
@@ -24,20 +21,20 @@ b: c
 	err := yaml.Unmarshal([]byte(yml), &node)
 	assert.NoError(t, err)
 
-	// Create a PositionFinderVisitor instance.
-	pv := &PositionFinderVisitor{}
+	pv := &PositionFinderVisitor{
+		position: protocol.Position{
+			Line:      2,
+			Character: 2,
+		},
+	}
 
 	// Walk the AST with the position finder visitor.
 	ast.Walk(pv, node)
 
-	// Check if the result is as expected.
-	expectedResult := "Hello "
-	if pv.result != expectedResult {
-		t.Errorf("Expected result: %q, got: %q", expectedResult, pv.result)
-	}
+	assert.True(t, pv.found)
+	assert.NotNil(t, pv.result)
 
-	// Check if the visitor found the target.
-	if !pv.found {
-		t.Error("Expected to find the target node, but it was not found.")
-	}
+	// Check if the result is as expected.
+	expectedResult := "World"
+	assert.Equal(t, expectedResult, pv.result.String())
 }
