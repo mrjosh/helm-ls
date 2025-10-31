41b9b364bd2bdc59516d5de8c29c783523a5aeae
qvalentin <valentin.theodor@web.de>
2025-10-31 12:56:33 +0100

    docs(nvim): update example for new nvim and nvim-lspconfig version

--- examples/nvim/init.lua
+++ examples/nvim/init.lua
@@ -5,14 +5,14 @@
 -- setup lazy plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
@@ -20,95 +20,60 @@ vim.g.mapleader = " "
 
 -- install required plugins
 require("lazy").setup({
-  -- use tree-sitter for syntax highlighting
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "master",
-    lazy = false,
-    build = ":TSUpdate",
-  },
-  -- alternativly you can use towolf/vim-helm for basic syntax highlighting
-  -- { "towolf/vim-helm",       ft = "helm" }, -- ft = 'helm' is important to not start yamlls
-  {
-    "qvalentin/helm-ls.nvim",
-    ft = "helm",
-    opts = {
-      conceal_templates = {
-        -- enable the replacement of templates with virtual text of their current values
-        enabled = true, -- tree-sitter must be setup for this feature
-      },
-      indent_hints = {
-        -- enable hints for indent and nindent functions
-        enabled = true, -- tree-sitter must be setup for this feature
-      },
-    },
-  },
-  { "neovim/nvim-lspconfig", event = { "BufReadPre", "BufNewFile", "BufEnter" } },
+	-- use tree-sitter for syntax highlighting
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "master",
+		lazy = false,
+		build = ":TSUpdate",
+	},
+	-- alternativly you can use towolf/vim-helm for basic syntax highlighting
+	-- { "towolf/vim-helm",       ft = "helm" }, -- ft = 'helm' is important to not start yamlls
+	{
+		"qvalentin/helm-ls.nvim",
+		ft = "helm",
+		opts = {
+			conceal_templates = {
+				-- enable the replacement of templates with virtual text of their current values
+				enabled = true, -- tree-sitter must be setup for this feature
+			},
+			indent_hints = {
+				-- enable hints for indent and nindent functions
+				enabled = true, -- tree-sitter must be setup for this feature
+			},
+		},
+	},
+	{ "neovim/nvim-lspconfig", event = { "BufReadPre", "BufNewFile", "BufEnter" } },
 })
 
-local lspconfig = require("lspconfig")
+local lspconfig = vim.lsp.config
+vim.lsp.enable("helm_ls")
 -- setup helm-ls
-lspconfig.helm_ls.setup({
-  settings = {
-    ["helm-ls"] = {
-      yamlls = {
-        path = "yaml-language-server",
-      },
-    },
-  },
+lspconfig("helm_ls", {
+	settings = {
+		["helm-ls"] = {
+			yamlls = {
+				path = "yaml-language-server",
+			},
+		},
+	},
 })
 
--- setup yamlls
-lspconfig.yamlls.setup({})
+-- enable yamlls
+vim.lsp.enable("yamlls")
+-- optional: configure yamlls options
+lspconfig("yamlls", {})
 
 -- setup treesitter for syntax highlighting
 require("nvim-treesitter.configs").setup({
-  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "yaml", "helm" },
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = true,
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-  highlight = {
-    enable = true,
-  },
-})
-
--- below are keymapping as recommended by nvim-lspconfig
-
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
-
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set("n", "<space>wl", function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<space>f", function()
-      vim.lsp.buf.format({ async = true })
-    end, opts)
-  end,
+	-- A list of parser names, or "all" (the listed parsers MUST always be installed)
+	ensure_installed = { "yaml", "helm" },
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = true,
+	-- Automatically install missing parsers when entering buffer
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
 })
